6101d1e31664d857d499339d873c14d6e1efed80
ariellenske <ariellenske@trentu.ca>
2023-10-14 20:04:47 -0700

    Initial commit

---
+++ R_scripts/functions/set_ggplot_custom_theme.R
@@ -0,0 +1,22 @@
+###############################################################################
+#Set ggplot custom theme
+#2016-06-02
+###############################################################################
+library(ggplot2)
+CustomTheme <- theme_set(theme_bw())
+CustomTheme <- theme_update(legend.key = element_rect(colour = NA), 
+                            legend.key.height = unit(1.2, "line"),
+                            panel.grid.major = element_line(colour = 'transparent'),
+                            panel.grid.minor = element_line(colour = 'transparent'),
+                            panel.border = element_rect(linetype = "solid",
+                                                        colour = "black",
+                                                        size = 1, fill = NA),
+                            axis.line = element_line(colour = "black"),
+                            strip.text = element_text(size = 12, colour = "black"),
+                            strip.background = element_rect(colour = "black",
+                                                            fill = "lightblue2",
+                                                            linetype = "solid"),
+                            axis.title.y = element_text(margin = margin(0,10,0,0)),
+                            axis.title.x = element_text(margin = margin(10,0,0,0)),
+                            panel.background = element_rect(fill = "white"))
+###############################################################################
