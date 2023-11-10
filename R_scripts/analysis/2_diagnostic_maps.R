23ed33f9d7f463552839848e68461fe3993a5513
Ariel <ariellenske@trentu.ca>
2023-11-09 22:42:27 -0800

    initial commit

---
+++ R_scripts/analysis/2_diagnostic_maps.R
@@ -0,0 +1,124 @@
+###############################################################################
+#project: BRAC gps data
+#2. map raw track data for each bird
+#date: Nov 09, 2023
+#author: Ariel Lenske
+###############################################################################
+
+#packages
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(viridis)
+library(patchwork)
+library(lubridate)
+
+#source functions
+source("R_scripts/functions/set_ggplot_custom_theme.R")
+source("R_scripts/functions/outputs_loc.R")
+source("R_scripts/functions/localgd_loc.R")
+
+#set data output base path to the projects google drive output folder
+outputbasepath <- outputs_loc("BRACgps_outputs")
+
+#set path to map_data folder
+mapdatapath <- localgd_loc("map_data")
+
+#load gps location data and deployment info
+gps <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_and_sensor_data.rds"))
+deploys <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_deployments.rds"))
+
+names(gps)
+names(deploys)
+
+#check num of unique birds, tags, and deployments
+length(unique(deploys$metalBand)) #30
+length(unique(deploys$tagID)) #30
+length(unique(deploys$deployID)) #30
+
+#list of deployment ids
+dlist <- deploys %>% dplyr::select(deployID) %>% distinct() %>% pull()
+
+#setup basic things for map 
+# land <- st_read(file.path(mapdatapath, "land_polygon", "ne_10m_land.shp")) #not detailed enough
+land <- st_read(file.path(mapdatapath, "world_coastline", "GSHHS_shp", "h", "GSHHS_h_L1.shp")) 
+eez <- st_read(file.path(mapdatapath, "eez", "eez.shp"))
+
+
+#plot each bird separately
+for(i in 1:length(dlist)){
+  
+  bird <- gps %>% filter(deployID == dlist[i]) 
+  dep <- deploys %>% filter(deployID == dlist[i]) 
+  
+  stime <- dep$deployTime - hours(4)
+  etime <- max(bird$ts) + hours(4)
+  
+  lims <- c(stime, etime)
+  
+  xmin <- min(min(bird$lon, na.rm = TRUE), min(dep$deployLon, na.rm = TRUE)) - 0.1
+  xmax <- max(max(bird$lon, na.rm = TRUE), max(dep$deployLon, na.rm = TRUE)) + 0.1
+  ymin <- min(min(bird$lat, na.rm = TRUE), min(dep$deployLat, na.rm = TRUE)) - 0.1
+  ymax <- max(max(bird$lat, na.rm = TRUE), max(dep$deployLat, na.rm = TRUE)) + 0.1
+  
+  
+  #map
+  p1 <- ggplot(data = eez) +
+    geom_sf(fill = "grey90", color = "grey90") +
+    geom_sf(data = land, color = "black", fill = "white") +
+    coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE) +
+    geom_point(data = bird,
+               aes(lon, lat, color = ts), alpha = 0.8, size = 1) +
+    geom_path(data = bird,
+              aes(lon, lat, color = ts), alpha = 0.8, linewidth = 0.75) +
+    geom_point(data = dep,
+               aes(deployLon, deployLat), color = "red", size = 2) +
+    scale_color_gradientn(name = "timestamp",
+                          colors = viridis(40, direction = -1, begin = 0, end = 1),
+                          trans = "time") +
+    theme(legend.position = "top") +
+    guides(colour = guide_colourbar(barwidth=25)) +
+    ylab("Latitude") + xlab("Longitude") +
+    ggtitle(paste0(dep$species, " ", dep$studySite,": tag",
+                   dep$tagID, " band", dep$metalBand,
+                   " year:", dep$year))
+  
+  #dives 
+  p2 <- ggplot(data = bird, aes(x = ts, y = barometricDepth, color = ts)) +
+    geom_point() +
+    scale_color_gradientn(name = "timestamp",
+                          colors = viridis(40, direction = -1, begin = 0, end = 1),
+                          trans = "time") +
+    scale_x_datetime(date_breaks = "1 day",
+                     date_minor_breaks = "1 hour",
+                     date_labels = "%d-%b",
+                     limits = lims,
+                     timezone = "America/Vancouver",
+                     expand = c(0, 0)) +
+    geom_point(data = dep, aes(x = deployTime, y = 0), color = "red", size = 2) +
+    geom_hline(yintercept = 0, color = "red") +
+    theme(legend.position = "none",
+          panel.grid.minor.x = element_line(colour="lightgrey", linewidth = 0.5),
+          panel.grid.major.x = element_line(colour="lightgrey", linewidth = 0.5)) +
+    guides(colour = guide_colourbar(barwidth=25)) +
+    ylab("Depth (meters)") + xlab("Time") +
+    scale_y_reverse()
+  
+  
+  png(filename = file.path(outputbasepath, "figs", "diagnostic-individual-bird-plots",
+                           paste0(dep$studySite,"-tag",
+                                  dep$tagID, "-", dep$year, ".png")),
+      width=10, height=9, units="in", res=300,
+      type = "cairo") 
+  
+  print(p1/p2 +
+          plot_layout(heights = c(2, 1)))
+  
+  dev.off()
+  
+  
+}
+rm(i)
+
+
+
---
+++ R_scripts/analysis/3_field_report_maps.R
@@ -0,0 +1,180 @@
+###############################################################################
+#project: BRAC gps data
+#5. field report maps
+#also contains code for some other maps e.g. for presentations
+#date: Nov 9, 2023
+#author: Ariel Lenske
+###############################################################################
+
+#packages
+library(tidyverse)
+library(lubridate)
+library(sf)
+library(viridis)
+library(terra)
+library(tidyterra)
+library(marmap)
+library(patchwork)
+
+#source functions
+source("R_scripts/functions/outputs_loc.R")
+source("R_scripts/functions/localgd_loc.R")
+source("R_scripts/functions/set_ggplot_custom_theme.R")
+
+#set data output base path to the projects google drive output folder
+outputbasepath <- outputs_loc("BRACgps_outputs")
+
+#set path to map_data folder
+mapdatapath <- localgd_loc("map_data")
+
+#Section 1: field report maps####
+
+#1.0 load gps location data and deployment info####
+gps <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_and_sensor_data.rds"))
+deploys <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_deployments.rds"))
+
+names(gps)
+names(deploys)
+
+#2.0 setup basic things for maping#### 
+
+#load map data
+bccoast = st_read(file.path(mapdatapath, "coastline_BC", "british_columbia_coastline.shp")) 
+eez = st_read(file.path(mapdatapath, "eez", "eez.shp"))
+bathy <- getNOAA.bathy(-124, -122, 50, 47, res = 0.2, keep = TRUE)
+
+#2.0 set up stuff for plotting bathymetry layer####
+#Switch to raster
+bathyr <- marmap::as.raster(bathy)
+bathyr <- rast(bathyr)
+plot(bathyr)
+
+#create color scheme for raster bathymetry map background
+# Function to calculate colour break points
+# x = raster, b1 & b2 = number of divisions for each sequence, r1 & r2 = rounding value
+colbr <- function(x, b1=50, b2=50, r1=-2, r2=-2) {
+  # Min/max values of the raster (x)
+  mi <- as.numeric(global(x, fun = "min")) - 100
+  ma <- as.numeric(global(x, fun = "max")) + 100
+  # Create sequences, but only use unique numbers
+  s1 <- unique(round(seq(mi, 0, 0-mi/b1),r1))
+  s2 <- unique(round(seq(0, ma, ma/b2),r2))
+  # Combine sequence for our break points, removing duplicate 0
+  s3 <- c(s1, s2[-1])
+  # Create a list with the outputs
+  # [[1]] = length of the first sequence minus 1 (water)
+  # [[2]] = length of the second sequence minus 1 (land)
+  # [[3]] = The break points
+  x <- list(length(s1)-1, length(s2)-1, s3)
+}
+
+mapbr <- colbr(x = bathyr, b1 = 12, b2 = 12)
+
+#colors
+ocean.col <- colorRampPalette(c("dodgerblue", "lightblue1"))
+land.col <- colorRampPalette(c("white"))
+
+#plot to check
+ggplot() +
+  geom_spatraster(data = bathyr) +
+  scale_fill_stepsn(breaks = mapbr[[3]], 
+                    colors = c(ocean.col(mapbr[[1]]), land.col(mapbr[[2]])), 
+                    values = scales::rescale(mapbr[[3]])) 
+
+
+#3.0 plot all birds together####
+site <- data.frame(studySite = deploys$studySite,
+                   deployLon = deploys$deployLon,
+                   deployLat = deploys$deployLat) %>%
+  distinct()
+
+
+xmin <- min(min(gps$lon, na.rm = TRUE)) - 0.1
+xmax <- max(max(gps$lon, na.rm = TRUE)) + 0.1
+ymin <- min(min(gps$lat, na.rm = TRUE)) - 0.1
+ymax <- max(max(gps$lat, na.rm = TRUE)) + 0.1
+
+#split gps data into alive and presumed or confirmed dead birds
+gps.a <- gps %>% 
+  dplyr::filter(!deployID %in% c("band112802265-tag210419-2023",
+                                 "band112802266-tag210418-2023",
+                                 "band112802269-tag220557-2023"))
+
+gps.d <- gps %>% 
+  dplyr::filter(deployID %in% c("band112802265-tag210419-2023",
+                                "band112802266-tag210418-2023",
+                                "band112802269-tag220557-2023"))
+
+#plot location data
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_gps_all.png"),
+    width=7, height=7, units="in", res=300,
+    type = "cairo") 
+
+ggplot() +
+  geom_spatraster(data = bathyr) +
+  scale_fill_stepsn(breaks = mapbr[[3]],
+                    colors = c(ocean.col(mapbr[[1]]), land.col(mapbr[[2]])),
+                    values = scales::rescale(mapbr[[3]])) +
+  geom_sf(data = eez, fill = "grey35", color = "grey35", size = 0.2, alpha = 0.1) +
+  geom_point(data = gps.a, aes(lon, lat, color = ts), alpha = 0.6, size = 0.3) +
+  geom_path(data = gps.a, aes(lon, lat, group = deployID, color = ts), alpha = 0.6, size = 0.3) +
+  geom_path(data = gps.d, aes(lon, lat, group = deployID),
+            color = "grey25", alpha = 0.6, size = 0.3, linetype = "dotted") +
+  geom_point(data = site,
+             aes(deployLon, deployLat), color = "red", size = 1) +
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  scale_color_gradientn(name = "timestamp",
+                        colors = viridis(40, direction = -1, begin = 0, end = 1),
+                        trans = "time") +
+  theme(legend.position = "top") +
+  guides(colour = guide_colourbar(barwidth=25),
+         fill = FALSE)
+
+dev.off()
+
+#4.0 plot dive data
+
+dives <-gps.a %>% dplyr::filter(!is.na(barometricDepth)) %>%
+  mutate(date = as.Date(ts))
+
+#plot data from all birds
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_dives_all.png"),
+    width=10, height=4, units="in", res=300,
+    type = "cairo") 
+
+ggplot() +
+  geom_point(data = dives,
+             aes(ts, barometricDepth, color = barometricDepth), alpha = 0.2, size = 0.3) +
+  scale_color_viridis(option = "B") +
+  theme(legend.position = "none") +
+  scale_y_reverse()
+
+dev.off()
+
+#plot dive data for one bird over one day as example
+
+#list of deployment ids
+dlist <- deploys %>% dplyr::select(deployID) %>% distinct() %>% pull()
+
+dives.sub <- dives %>% 
+  dplyr::filter(deployID == dlist[1] & ts > as.POSIXct("2023-10-16 8:30") & ts < as.POSIXct("2023-10-16 9:00"))
+
+
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_dive_example.png"),
+    width=10, height=4, units="in", res=300,
+    type = "cairo") 
+
+ggplot() +
+  geom_point(data = dives.sub,
+             aes(ts, barometricDepth, color = barometricDepth), alpha = 0.6, size = 0.3) +
+  ylab("depth (m)") + xlab("time") +
+  scale_color_viridis(option = "B", direction = -1) +
+  scale_y_reverse() +
+  geom_hline(yintercept = 0, linetype = "longdash", color = "grey")
+
+dev.off()
+
+
+
