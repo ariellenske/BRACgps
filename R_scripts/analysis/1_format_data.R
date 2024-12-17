2b089d1a5b1b0d80040aa72daf653f0b9e463306
Lenske <Ariel.Lenske@ec.gc.ca>
2024-12-16 21:55:23 -0800

    updated code to work with move2

--- R_scripts/analysis/1_format_data.R
+++ R_scripts/analysis/1_format_data.R
@@ -8,8 +8,9 @@
 #packages
 library(tidyverse)
 library(lubridate)
-library(move)
-library(keyring)
+library(move2)
+# library(keyring)
+library(sf)
 
 #source functions
 source("R_scripts/functions/outputs_loc.R")
@@ -20,67 +21,43 @@ outputbasepath <- outputs_loc("BRACgps_outputs")
 #load data from movebank
 
 #store movebank password
-key_set(service = "Movebank",
-        username = "ariellenske",
-        prompt = "Movebank password: ")
-
-#set movebank login
-mb_user <- keyring::key_list(service = "Movebank")$username
-mb_pass <- keyring::key_get("Movebank", username = mb_user)
-login <- move::movebankLogin(username = mb_user, password = mb_pass)
+movebank_store_credentials(username = "ariellenske")
 
 #load data from BRAC gps movebank project 
 #3066962629 - Brandt's Cormorant BRAC, Salish Sea BC Canada (GPS)
 
 #get the animal names of the study
-birdIDs <- as.character(unique(getMovebankAnimals(study = 3066962629, login = login)$local_identifier))
+birdIDs <- as.character(unique(movebank_download_deployment(study = 3066962629)$individual_local_identifier))
 
-#gps and associated sensor data
-#download each individual's data and create a MoveStack
+#get list of study senso type ids
+movebank_download_study_info(study_id = 3066962629)$sensor_type_ids
+
+#get entity type list (gives you the correct formatting for sensor_type_id)
+movebank_retrieve(entity_type = "tag_type")
+
+#gps and sensor data
+#download each individual's data and combine in one df
 locs <- lapply(birdIDs, function(x){
   
   print(paste0(x," (", match(x, birdIDs), " of ", length(birdIDs),")"))
-  getMovebankData(study = 3066962629,
-                  login = login, 
-                  animalName = x, 
-                  removeDuplicatedTimestamps = TRUE,
-                  includeExtraSensors = FALSE,
-                  includeOutliers = FALSE)
+  
+  movebank_download_study(study_id = 3066962629,
+                               sensor_type_id = c("gps", "accessory-measurements"),
+                               individual_local_identifier = x, 
+                               remove_movebank_outliers = TRUE,
+                               convert_spatial_columns = FALSE,)
 
   })
 
-locs <- moveStack(locs, forceTz = "UTC")
+#convert list to df (spatial point sfc)
+db <- bind_rows(locs)
 
-#convert to df
-locs.df <- as(locs, 'data.frame') 
-
-
-#sensor data with no location
-#download each individual's data and create a MoveStack
-sensors <- lapply(birdIDs, function(x){
-  
-  print(paste0(x," (", match(x, birdIDs), " of ", length(birdIDs),")"))
-  getMovebankNonLocationData(study = 3066962629,
-                             login = login, 
-                             animalName = x)
-  
-})
-
-
-# sensors <- getMovebankNonLocationData(study = 3066962629, login = login)
-
-#add deployId to sensor records
-deployIDs <- locs.df %>% 
-  dplyr::select(study_site, deployment_local_identifier, tag_local_identifier,
-                individual_local_identifier = ring_id) %>%
-  distinct()
-
-sensors <- left_join(sensors, deployIDs)
-
-#combine gps and sensor data into one dataframe
-db <- bind_rows(locs.df %>% rename(individual_local_identifier = ring_id),
-                sensors)
-
+#convert to flat table with all attributes
+db <- mt_as_event_attribute(db, names(mt_track_data(db)))
+db <- dplyr::mutate(db, 
+                      location_long = sf::st_coordinates(db)[,1],
+                      location_lat = sf::st_coordinates(db)[,2])
+db <- sf::st_drop_geometry(db)
 
 #clean up dataset
 
@@ -91,29 +68,48 @@ sciname.df <- data.frame(species = c("BRAC"),
 db <- left_join(db, sciname.df)
 
 #list of cols want to keep
-cols <- c("study_site", "timestamp", "location_lat", "location_long",
-          "acceleration_raw_x", "acceleration_raw_y","acceleration_raw_z",
-          "magnetic_field_raw_x", "magnetic_field_raw_y", "magnetic_field_raw_z",         
-          "ground_speed", "heading","height_above_msl",
-          "barometric_height","barometric_depth", 
-          "external_temperature", "light_level",
-          "tag_mass_total", "tag_readout_method",
+cols <- c("study_site",
+          "event_id",
+          "timestamp", 
+          "location_lat", "location_long",
+          "acceleration_raw_x", "acceleration_raw_y", "acceleration_raw_z",
+          "magnetic_field_raw_x","magnetic_field_raw_y","magnetic_field_raw_z",         
+          "ground_speed",
+          "heading",
+          "height_above_msl",
+          "barometric_height",
+          "barometric_depth", 
+          "external_temperature", 
+          "light_level",
+          "tag_mass_total", 
+          "tag_readout_method",
           "tag_voltage", "battery_charge_percent","battery_charging_current",
-          "gps_satellite_count", "gps_time_to_fix", "gps_hdop", 
+          "gps_satellite_count", 
+          "gps_time_to_fix", 
+          "gps_hdop", 
+          "ornitela_transmission_protocol",
           "deployment_local_identifier",
-          "tag_local_identifier", "individual_local_identifier", "species", "taxon_canonical_name",
-          "animal_mass", "animal_reproductive_condition", "sex",
+          "tag_local_identifier", 
+          "manufacturer_name",
+          "model",
+          "individual_local_identifier", 
+          "species", "taxon_canonical_name",
+          "animal_mass", 
+          "animal_life_stage",
+          "animal_reproductive_condition", 
+          # "sex",
           "attachment_type", 
-          "deploy_on_person", "deploy_on_timestamp", 
+          "deploy_on_person", 
+          "deploy_on_timestamp", 
           "deploy_on_latitude", "deploy_on_longitude",
-          "duty_cycle", "comments.y")
+          "duty_cycle",
+          "deployment_comments")
 
-
-                                
+                               
 db <- db %>%
   dplyr::select(!!!rlang::syms(cols)) %>%
   dplyr::mutate(
-    timestamp = with_tz(as.POSIXct(timestamp, tz = "UTC"), tzone = "America/Vancouver"), # convert times to PDT
+    timestamp = with_tz(as.POSIXct(timestamp, tz = "UTC"), tzone = "America/Vancouver"), # convert times to PT
     deploy_on_timestamp = with_tz(as.POSIXct(deploy_on_timestamp, tz = "UTC"), tzone = "America/Vancouver"),
     year = as.numeric(strftime(timestamp, '%Y')), #add year field
     month = as.numeric(strftime(timestamp, '%m')) # add numeric month field
@@ -130,7 +126,7 @@ db <- db %>%
          attachmentType = attachment_type,
          deployLat = deploy_on_latitude,
          deployLon = deploy_on_longitude,
-         deployComments = comments.y,
+         deployComments = deployment_comments,
          deployID = deployment_local_identifier,
          fixRate = duty_cycle,
          studySite = study_site,
@@ -142,39 +138,43 @@ db <- db %>%
          acceleration_x = acceleration_raw_x, 
          acceleration_y = acceleration_raw_y, 
          acceleration_z = acceleration_raw_z, 
-         magneticField_x = magnetic_field_raw_x,
-         magneticField_y = magnetic_field_raw_y,
-         magneticField_z = magnetic_field_raw_z,
+         magnetic_x = magnetic_field_raw_x,
+         magnetic_y = magnetic_field_raw_y,
+         magnetic_z = magnetic_field_raw_z,
          groundSpeed = ground_speed,        
          barometricHeight = barometric_height,
          barometricDepth = barometric_depth,
          temperature = external_temperature,
          lightLevel = light_level)
 
+#remove attributes from df
+db <- as.data.frame(db)
 
 #split into gps/sensor data and deployment info
 gps <- db %>% 
-  dplyr::select(studySite, species, deployID, tagID, metalBand, ts, lat, lon, 
+  dplyr::select(studySite, species, deployID, tagID, metalBand, event_id, ts, lat, lon, 
                 acceleration_x, acceleration_y, acceleration_z,
-                magneticField_x, magneticField_y, magneticField_z, groundSpeed, heading,                 
+                magnetic_x, magnetic_y, magnetic_z, 
+                groundSpeed, heading,                 
                 height_above_msl, barometricHeight, barometricDepth,
-                temperature, lightLevel, tagVoltage, battery_charge_percent, battery_charging_current,
-                num_satellites, gps_time_to_fix, gps_hdop,
+                temperature, lightLevel, 
+                tagVoltage, battery_charge_percent, battery_charging_current,
+                num_satellites, gps_time_to_fix, gps_hdop, ornitela_transmission_protocol,
                 year, month) %>%
-  arrange(studySite, year, species, deployID, ts)
+  group_by(deployID) %>%
+  arrange(ts) %>%
+  ungroup()
 
 deploys <- db %>% 
-  dplyr::select(studySite, deployLat, deployLon, year, deployID, deployTime, 
-                tagID, metalBand, species, speciesSciName, animal_reproductive_condition,
+  dplyr::select(studySite, deployLat, deployLon, deployID, deployTime, 
+                tagID, manufacturer_name, model, metalBand, species, speciesSciName, animal_reproductive_condition,
                 deployMass, attachmentType, tag_mass_total, tag_readout_method, deploy_on_person,
                 deployComments) %>%
-  distinct() %>%
-  dplyr::filter(!is.na(deployID)) %>%
-  arrange(studySite, year, species, deployID)
+  distinct() 
  
   
 #save dataframes to data_working folder on google drive
 saveRDS(gps, file.path(outputbasepath, "data_working", "brac_gps_and_sensor_data.rds"))
-saveRDS(deploys, file.path(outputbasepath, "data_working", "brac_gps_deployments.rds"))
+saveRDS(deploys, file.path(outputbasepath, "data_working", "brac_deployments.rds"))
 
 
