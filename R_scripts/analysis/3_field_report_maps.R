70a1fa442399fb42cea6c62c1a9440f196ff5e38
Lenske <Ariel.Lenske@ec.gc.ca>
2026-01-21 19:12:44 -0800

    added some new plots and updated others

--- R_scripts/analysis/3_field_report_maps.R
+++ R_scripts/analysis/3_field_report_maps.R
@@ -15,7 +15,7 @@ library(terra)
 library(tidyterra)
 library(marmap)
 library(patchwork)
-library(moveVis)
+# library(moveVis)
 
 #source functions
 source("R_scripts/functions/outputs_loc.R")
@@ -32,7 +32,7 @@ mapdatapath <- localgd_loc("map_data")
 
 #1.0 load gps location data and deployment info####
 gps <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_and_sensor_data.rds"))
-deploys <- readRDS(file.path(outputbasepath, "data_working", "brac_gps_deployments.rds"))
+deploys <- readRDS(file.path(outputbasepath, "data_working", "brac_deployments.rds"))
 
 names(gps)
 names(deploys)
@@ -42,7 +42,7 @@ names(deploys)
 #load map data
 bccoast = st_read(file.path(mapdatapath, "coastline_BC", "british_columbia_coastline.shp")) 
 eez = st_read(file.path(mapdatapath, "eez", "eez.shp"))
-bathy <- getNOAA.bathy(-124, -122, 50, 47, res = 0.25, keep = TRUE)
+bathy <- getNOAA.bathy(-125, -122, 50, 45, res = 0.25, keep = TRUE)
 coast <- sf::read_sf(file.path(mapdatapath, "world_coastline", "GSHHS_shp", "f", "GSHHS_f_L1.shp"))
 
 
@@ -93,46 +93,83 @@ site <- data.frame(studySite = deploys$studySite,
                    deployLat = deploys$deployLat) %>%
   distinct()
 
-
-xmin <- min(min(gps$lon, na.rm = TRUE)) - 0.1
-xmax <- max(max(gps$lon, na.rm = TRUE)) + 0.1
-ymin <- min(min(gps$lat, na.rm = TRUE)) - 0.1
-ymax <- max(max(gps$lat, na.rm = TRUE)) + 0.1
-
-#split gps data into alive and presumed or confirmed dead birds
-gps.a <- gps %>% 
-  dplyr::filter(!deployID %in% c("band112802265-tag210419-2023",
-                                 "band112802266-tag210418-2023",
-                                 "band112802269-tag220557-2023",
-                                 "band112802277-tag220552-2023",
-                                 "band112802270-tag210421-2023",
-                                 "band112802257-tag210551-2023",
-                                 "band112802268-tag210552-2023"))
-
-gps.d <- gps %>% 
-  dplyr::filter(deployID %in% c("band112802265-tag210419-2023",
-                                "band112802266-tag210418-2023",
-                                "band112802269-tag220557-2023",
-                                "band112802277-tag220552-2023",
-                                "band112802270-tag210421-2023",
-                                "band112802257-tag210551-2023",
-                                "band112802268-tag210552-2023"))
-
 #plot location data
-png(filename = file.path(outputbasepath, "figs", "reports", "brac_gps_all.png"),
-    width=7, height=7, units="in", res=300,
-    type = "cairo") 
+#pull out gps data (remove records with only sensor data)
+gpso <- gps %>% dplyr::filter(!is.na(lat)) %>% dplyr::select(deployID, ts, tagID, lat, lon)
 
-ggplot() +
+#################################
+#segment tracks so that don't get lines between fixes that are far apart in time (i.e. accross large gaps btw fixes)
+gpso <- gpso %>% 
+  group_by(deployID) %>%
+  arrange(ts) %>%
+  mutate(lagts = lag(ts),
+         timediff = signif(difftime(ts, lagts, units = "hours"), digits = 2),
+         gap = ifelse(timediff <= 3, 0, 1) #gap defined as > 3 hours
+  ) %>%
+  ungroup()
+
+
+#trackseg ID function
+add_tracksegID <- function(bird){
+  
+  bird <- bird[with(bird, order(ts)), ]
+  
+  gap <- bird$gap
+  tracksegID <- rep(0, nrow(bird))
+  tracksegID[1] <- 1
+  
+  for(i in 2:nrow(bird)){
+    
+    if(gap[i] == 1){
+      
+      tracksegID[i] <- tracksegID[i-1] + 1
+      
+    } else if(gap[i] == 0){
+      
+      tracksegID[i] <- tracksegID[i-1]
+    }}
+  
+  bird <- bird %>% mutate(tracksegID = tracksegID)
+  
+}
+
+#add segmentIDs using above function
+gpso <- gpso %>%
+  group_by(deployID) %>%
+  do(add_tracksegID(.)) %>%
+  ungroup() 
+
+#remove extra columns
+gpso <- gpso %>% dplyr::select(-lagts, -timediff, -gap)
+
+#make tripsegID unique
+gpso <- gpso %>% mutate(tracksegID = paste0(deployID, "-", tracksegID))
+
+#get summary of number of fixes per track segment
+tab <- gpso %>%
+  group_by(tracksegID) %>%
+  summarise(n = n())
+#################################
+
+#subset gps data to with and without bird that went to Oregon
+gpso1 <- gpso %>% dplyr::filter(tagID == "210424")
+gpso0 <- gpso %>% dplyr::filter(tagID != "210424")
+
+#plot all birds except for "210424"
+#set plot boundaries
+xmin <- min(min(gpso0$lon, na.rm = TRUE)) - 0.1
+xmax <- max(max(gpso0$lon, na.rm = TRUE)) + 0.1
+ymin <- min(min(gpso0$lat, na.rm = TRUE)) - 0.1
+ymax <- max(max(gpso0$lat, na.rm = TRUE)) + 0.1
+
+p1 <- ggplot() +
   geom_spatraster(data = bathyr) +
   scale_fill_stepsn(breaks = mapbr[[3]],
                     colors = c(ocean.col(mapbr[[1]]), land.col(mapbr[[2]])),
                     values = scales::rescale(mapbr[[3]])) +
   geom_sf(data = eez, fill = "grey35", color = "grey35", size = 0.2, alpha = 0.1) +
-  geom_point(data = gps.a, aes(lon, lat, color = ts), alpha = 0.6, size = 0.3) +
-  geom_path(data = gps.a, aes(lon, lat, group = deployID, color = ts), alpha = 0.6, size = 0.3) +
-  geom_path(data = gps.d, aes(lon, lat, group = deployID),
-            color = "grey25", alpha = 0.6, size = 0.3, linetype = "dotted") +
+  geom_point(data = gpso0, aes(lon, lat, color = ts), alpha = 0.6, size = 0.3) +
+  geom_path(data = gpso0, aes(lon, lat, group = tracksegID, color = ts), alpha = 0.6, linewidth = 0.3) +
   geom_point(data = site,
              aes(deployLon, deployLat), color = "red", size = 1) +
   coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE) +
@@ -142,24 +179,64 @@ ggplot() +
                         colors = viridis(40, direction = -1, begin = 0, end = 1),
                         trans = "time") +
   theme(legend.position = "top") +
-  guides(colour = guide_colourbar(barwidth=25),
+  guides(colour = guide_colourbar(barwidth = 25),
          fill = FALSE)
+
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_gps_all_local.png"),
+    width=7, height=7, units="in", res=300,
+    type = "cairo") 
+
+print(p1)
 
 dev.off()
 
-#4.0 plot dive data
+#plot "210424"
+#set plot boundaries
+xmin <- min(min(gpso1$lon, na.rm = TRUE)) - 0.1
+xmax <- max(max(gpso1$lon, na.rm = TRUE)) + 0.1
+ymin <- min(min(gpso1$lat, na.rm = TRUE)) - 0.1
+ymax <- max(max(gpso1$lat, na.rm = TRUE)) + 0.1
 
-dives <-gps.a %>% dplyr::filter(!is.na(barometricDepth)) %>%
-  mutate(date = as.Date(ts))
+p2 <- ggplot() +
+  geom_spatraster(data = bathyr) +
+  scale_fill_stepsn(breaks = mapbr[[3]],
+                    colors = c(ocean.col(mapbr[[1]]), land.col(mapbr[[2]])),
+                    values = scales::rescale(mapbr[[3]])) +
+  geom_sf(data = eez, fill = "grey35", color = "grey35", size = 0.2, alpha = 0.1) +
+  geom_point(data = gpso1, aes(lon, lat), alpha = 0.6, size = 0.3, color = "black") +
+  geom_path(data = gpso1, aes(lon, lat, group = tracksegID), alpha = 0.6, linewidth = 0.3, color = "black") +
+  geom_point(data = site,
+             aes(deployLon, deployLat), color = "red", size = 1) +
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  scale_color_gradientn(name = "timestamp",
+                        colors = viridis(40, direction = -1, begin = 0, end = 1),
+                        trans = "time") +
+  theme(legend.position = "none") 
+
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_gps_210424.png"),
+    width=7, height=14, units="in", res=300,
+    type = "cairo") 
+
+print(p1)
+
+dev.off()
+
+
+#4.0 plot dive data
+dives <- gps %>% dplyr::filter(!is.na(barometricDepth)) %>%
+  mutate(date = as.Date(ts),
+         barometricDepth2 = as.numeric(barometricDepth)) 
 
 #plot data from all birds
-png(filename = file.path(outputbasepath, "figs", "reports", "brac_dives_all.png"),
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_dives_all_v2.png"),
     width=10, height=4, units="in", res=300,
     type = "cairo") 
 
 ggplot() +
   geom_point(data = dives,
-             aes(ts, barometricDepth, color = barometricDepth), alpha = 0.2, size = 0.3) +
+             aes(ts, barometricDepth2, color = barometricDepth2), alpha = 0.2, size = 0.3) +
   scale_color_viridis(option = "B") +
   theme(legend.position = "none") +
   scale_y_reverse()
@@ -174,20 +251,50 @@ dlist <- deploys %>% dplyr::select(deployID) %>% distinct() %>% pull()
 dives.sub <- dives %>% 
   dplyr::filter(deployID == dlist[1] & ts > as.POSIXct("2023-10-16 8:30") & ts < as.POSIXct("2023-10-16 9:00"))
 
-
-png(filename = file.path(outputbasepath, "figs", "reports", "brac_dive_example.png"),
-    width=10, height=4, units="in", res=300,
-    type = "cairo") 
-
-ggplot() +
+p3 <- ggplot() +
   geom_point(data = dives.sub,
-             aes(ts, barometricDepth, color = barometricDepth), alpha = 0.6, size = 0.3) +
+             aes(ts, barometricDepth2, color = barometricDepth2), alpha = 0.6, size = 0.3) +
   ylab("depth (m)") + xlab("time") +
   scale_color_viridis(option = "B", direction = -1) +
   scale_y_reverse() +
   geom_hline(yintercept = 0, linetype = "longdash", color = "grey")
 
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_dive_example.png"),
+    width=10, height=4, units="in", res=300,
+    type = "cairo") 
+
+print(p3)
+
 dev.off()
+
+p4 <- ggplot() +
+  geom_point(data = dives.sub,
+             aes(ts, barometricDepth2), alpha = 0.6, size = 0.3, color = "grey30") +
+  ylab("depth (m)") + xlab("time") +
+  scale_color_viridis(option = "B", direction = -1) +
+  scale_y_reverse() +
+  geom_hline(yintercept = 0, linetype = "longdash", color = "grey")
+
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_dive_example_grey.png"),
+    width=10, height=4, units="in", res=300,
+    type = "cairo") 
+
+print(p4)
+
+dev.off()
+
+
+#multi plot figure
+png(filename = file.path(outputbasepath, "figs", "reports", "brac_overall_plot.png"),
+    width=13, height=10, units="in", res=300,
+    type = "cairo") 
+
+print((p1 / p4 + plot_layout(heights = c(2, 1))) | p2) +
+  plot_annotation(tag_levels = "A")
+
+dev.off()
+
+
 
 #5.0 animate GPS tracks####
 
@@ -282,6 +389,6 @@ frames[[40]] # preview one of the frames
 
 # animate frames (slow ~20min)
 animate_frames(frames, 
-               out_file = "animated_BRAC_Salish_Sea.gif",
+               out_file = "animated_BRAC_Salish_Sea_2.gif",
                overwrite = TRUE,
-               fps = 25)
+               fps = 100)
